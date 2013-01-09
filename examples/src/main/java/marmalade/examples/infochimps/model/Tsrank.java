0ea8cd534888bd4c73c4a15ed0b411682f779237
Marc Fornós <marc.fornos@gmail.com>
2013-01-09 23:06:44 +0100

    total rename

--- examples/src/main/java/marmalade/examples/infochimps/model/Tsrank.java
+++
@@ -1,7 +0,0 @@
-package marmalade.examples.infochimps.model;
-
-public class Tsrank extends Social
-{
-   public Float trstrank;
-   public Integer tq;
-}
--- marmalade-core/src/main/java/marmalade/mime/ContentTypeEx.java
+++
@@ -1,8 +0,0 @@
-package marmalade.mime;
-
-import org.apache.http.entity.ContentType;
-
-public interface ContentTypeEx
-{
-   public static final ContentType SMILE = ContentType.create("application/x-jackson-smile");
-}
